7caf53ed1c0b2df4435ab43137dceb16b0f820f6
ghost <mac@macs-Laptop.local>
2024-04-22 23:32:42 +0400

    .files added

---
+++ nvim/lua/options.lua
@@ -0,0 +1,25 @@
+require "nvchad.options"
+
+-- add yours here!
+local o = vim.o
+
+ -- local o = vim.o
+o.relativenumber = true
+o.number = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+o.autoindent = true
+o.mouse = "a"
+
+o.wrap = false
+o.ignorecase = true
+o.smartcase = true
+o.cursorline = true
+
+o.backspace = "indent,eol,start"
+
+o.splitright = true
+o.splitbelow = true
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
